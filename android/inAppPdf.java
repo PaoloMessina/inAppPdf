d842f4847bfa9d1ee1df17c29d6897ced057f6bc
Paolo Messina <phemt.latd@gmail.com>
2015-07-08 16:48:47 +0200

    Android platform
    Start android development

---
+++ android/inAppPdf.java
@@ -0,0 +1,48 @@
+package it.almaviva.cordovaplugins.inapppdf;
+
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class inAppPdf extends CordovaPlugin {
+	/**
+     * Constructor.
+     */
+    public inAppPdf() {
+    }
+
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+    
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action            The action to execute.
+     * @param args              JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return                  True if the action was valid, false if not.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("show")) {
+            Toast toast = Toast.makeText(this.cordova.getActivity().getApplicationContext();, "ciao", Toast.LENGTH_SHORT);
+			toast.show();
+            callbackContext.success(r);
+        }
+        else {
+            return false;
+        }
+        return true;
+    }
+}
