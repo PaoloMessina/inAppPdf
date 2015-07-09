5ac64d2de21b03394ac38b6beaf4b820cb103e68
Paolo Messina <phemt.latd@gmail.com>
2015-07-09 10:12:34 +0200

    git commit
    commit git

--- android/inAppPdf.java
+++ android/inAppPdf.java
@@ -37,8 +37,35 @@ public class InAppPdf extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("show")) {
         	JSONObject r = new JSONObject();
-            //Toast toast = Toast.makeText(this.cordova.getActivity().getApplicationContext();, "ciao", Toast.LENGTH_SHORT);
-			//toast.show();
+        	
+        	File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() +"/"+ filename);
+			Intent target = new Intent(Intent.ACTION_VIEW);
+			target.setDataAndType(Uri.fromFile(file),"application/pdf");
+			target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+
+			Intent intent = Intent.createChooser(target, "Open File");
+			try {
+    			startActivity(intent);
+			} catch (ActivityNotFoundException e) {
+    			// No application to view, ask to download one
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("No Viewer");
+                builder.setMessage("Download PDF Viewer?");
+                builder.setPositiveButton(getString("Okay"),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                    int which) {
+                                Intent innerIntent = new Intent(
+                                        Intent.ACTION_VIEW);
+                                innerIntent.setData(Uri
+                                        .parse("market://details?id=com.adobe.reader"));
+                                startActivity(innerIntent);
+                            }
+                        });
+                builder.setNegativeButton("Cancel"), null);
+                builder.create().show();
+			}
             callbackContext.success(r);
         }
         else {
