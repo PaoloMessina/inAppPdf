c670dd3dd1f34181f1a4be6291dcae812758f00f
Paolo Messina <phemt.latd@gmail.com>
2015-07-21 11:39:01 +0200

    Commit changes
    Commit changes to inAppPdf, android reader to file data folder

--- android/inAppPdf.java
+++ android/inAppPdf.java
@@ -5,8 +5,10 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
 
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
@@ -17,6 +19,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class InAppPdf extends CordovaPlugin {
 
@@ -48,12 +53,37 @@ public class InAppPdf extends CordovaPlugin {
      */
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("show")) {
-        	JSONObject r = new JSONObject();
+        	JSONObject r = args.getJSONObject(0);
+            String targetPath = r.optString("targetPath");
             c = this.cordova.getActivity().getApplicationContext();
+
+            AssetManager assetManager = c.getAssets();
+
+            InputStream in = null;
+            OutputStream out = null;
+            //File file = new File(c.getFilesDir(), "abc.pdf");
+            //File file = new File(targetPath);
+            /*try
+            {
+                in = assetManager.open("abc.pdf");
+                out = c.openFileOutput(file.getName(), Context.MODE_WORLD_READABLE);
+
+                copyFile(in, out);
+                in.close();
+                in = null;
+                out.flush();
+                out.close();
+                out = null;
+            } catch (Exception e)
+            {
+                Log.e("tag", e.getMessage());
+            }*/
+
+
         	
-        	File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() +"/"+ "sample.pdf");
+        	//file = new File("file://" + c.getFilesDir() + "/abc.pdf");
 			Intent target = new Intent(Intent.ACTION_VIEW);
-			target.setDataAndType(Uri.fromFile(file),"application/pdf");
+			target.setDataAndType(Uri.parse(targetPath),"application/pdf");
 			target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
             target.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
@@ -88,5 +118,15 @@ public class InAppPdf extends CordovaPlugin {
             return false;
         }
         return true;
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException
+    {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1)
+        {
+            out.write(buffer, 0, read);
+        }
     }
 }
